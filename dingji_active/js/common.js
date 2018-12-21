a3d90cfcf9a467433c6303c3c4b51cadcd8e7475
VIPsth <VIPsongtaohong@yeah.net>
2018-12-21 10:53:55 +0800

    add

--- dingji_active/js/common.js
+++ dingji_active/js/common.js
@@ -9,6 +9,12 @@ var response = function() {
  	this.responseTimer = setTimeout(response, 300);
  };
  response();
+ if(window.location.href.indexOf('yaoqing_id/')!=-1){
+	window.location.href=window.location.href.replace('yaoqing_id/','yaoqing_id=')
+ }
+ if(window.location.href.indexOf('fenxiang_id/')!=-1){
+	window.location.href=window.location.href.replace('fenxiang_id/','fenxiang_id=')
+ }
 if (window.location.href.charAt(window.location.href.length - 1) == "=") {
 	let locationHref = window.location.href.substring(0, window.location.href.length - 1);
 	window.location.href = locationHref;
